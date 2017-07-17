2289797df576579369a4a4fb8464197aa4c3c55b
zuiko <zuiko@DESKTOP-LRRB9CG>
2017-07-17 14:26:54 +0300

    Add project files.

---
+++ Tecwi.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tecwi.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
